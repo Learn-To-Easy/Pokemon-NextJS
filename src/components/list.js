d93edbb5c7574a8dfaed7c8c66a89b322a86f76d
xrzky <lukirizkii18@gmail.com>
2024-05-01 22:45:47 +0700

    fix img to Image from nextjs and add loading

--- src/components/list.js
+++ src/components/list.js
@@ -6,14 +6,18 @@ import usePokemon from '@/hooks/usePokemon';
 import Image from "next/image";
 
 export default function list() {
-  const { filteredPokemon, searchQuery, handleSearchChange } = usePokemon();
+  const { filteredPokemon, searchQuery, handleSearchChange, isLoading, isNotFound } = usePokemon();
 
   return (
     <>
       <div className="mx-20 py-auto ">
         <div className="relative">
           <div className="absolute inset-y-0 flex items-center start-0 ps-3">
-            <img src="./search.svg" alt="search-icon" />
+            <Image
+            src="./search.svg" 
+            width={20}
+            height={20}
+            alt="search-icon"/>
           </div>
           <div className="flex">
             <input
@@ -25,12 +29,19 @@ export default function list() {
               onChange={handleSearchChange}
               required
             />
-            <img className="p-2 mx-4 bg-white rounded-full" src="./text_format.svg" alt="" />
+            <Image
+            className="p-2 mx-4 bg-white rounded-full" 
+            src="./text_format.svg"
+            width={36}
+            height={36}
+            alt=""/>
           </div>
         </div>
       </div>
       <div className="py-5 mx-5">
         <div className="min-h-screen pl-4 overflow-y-auto bg-white shadow-inner rounded-xl max-h-24">
+        { isLoading && <p className='text-center'>Loading...</p>}
+        { !isLoading && (
           <div className="flex flex-wrap items-center justify-center gap-5 mx-0 my-4">
             { filteredPokemon.map ((pokemon) => (
               <div key={ pokemon.id }>
@@ -39,11 +50,10 @@ export default function list() {
                     <div className="px-2 py-2 text-right text-grey-text min-h-4">
                       <p className="text-[12px] leading-3">#{String( pokemon.id ).padStart(3, "0")}</p>
                     </div>
-                    <div className="w-20 h-20 m-auto">
-                      <Image className="w-full h-full"
+                    <div className="relative w-20 h-20 m-auto">
+                      <Image
                         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemon.id }.svg`}
-                        width={100}
-                        height={100}
+                        layout='fill'
                         alt={ pokemon.name } />
                     </div>
                     <div className="rounded-lg bg-grey-bg pt-7 pr-2 pb-3 pl-2 mt-[-18px] text-title-list">
@@ -53,10 +63,11 @@ export default function list() {
                 </Link>
               </div>
             ))}
-            { filteredPokemon.length === 0 && (
+             { filteredPokemon.length === 0 && (
               <div className="text-center">Pokemon tidak ditemukan</div>
             )}
           </div>
+          )}
         </div>
       </div>
     </>
--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -4,26 +4,28 @@ import { getPokemonData } from '@/services/pokeApi';
 const usePokemon = () => {
     const [pokemonData, setPokemonData] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
+    const [isLoading, setLoading] = useState(true);
 
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value);
-      };
-      
-      const pokemonWithIds = pokemonData.map((pokemon, index) => ({
+    };
+
+    const pokemonWithIds = pokemonData.map((pokemon, index) => ({
         ...pokemon,
         id: index + 1
-      }));
-    
-      const filteredPokemon = pokemonWithIds.filter((pokemon) =>
+    }));
+
+    const filteredPokemon = pokemonWithIds.filter((pokemon) =>
         pokemon.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
         pokemon.id.toString().padStart(3, '0').includes(searchQuery.toLowerCase())
-      );
-    
+    );
+
     useEffect(() => {
         const getData = async () => {
             try {
                 const data = await getPokemonData();
                 setPokemonData(data);
+                setLoading(false)
             } catch (error) {
                 console.log(error);
             }
@@ -33,7 +35,7 @@ const usePokemon = () => {
 
     }, []);
 
-    return { pokemonData, searchQuery, handleSearchChange, filteredPokemon };
+    return { pokemonData, searchQuery, handleSearchChange, filteredPokemon, isLoading };
 };
 
 export default usePokemon;
